706bfcff1670c4da7f6432ad60a788c8e6682a73
Hector Valenzuela <hypermente@hotmail.com>
2024-12-02 18:27:40 -0500

    Updated auth and books

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -58,8 +58,14 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.session.authorization.username;
+  console.log(review);
+
+  books[isbn].reviews[username] = review;
+
+  res.send("Review '" + review + "' of '" + username + "' stored");
 });
 
 module.exports.authenticated = regd_users;
