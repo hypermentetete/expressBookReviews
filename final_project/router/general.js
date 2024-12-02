90377cf4c4d46b68924d07d12148af021b1ca0d5
Hector Valenzuela <hypermente@hotmail.com>
2024-12-02 10:10:20 -0500

    Restored port 5000; added get by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,22 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //get all books
-  res.send(JSON.stringify(books, null, 4));
+  res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //get specific book
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //get books of author
+  const arrayBooks = Object.values(books);
+  const authorBooks = arrayBooks.filter((book) => {return book.author === req.params.author})
+  res.send(authorBooks);
 });
 
 // Get all books based on title
