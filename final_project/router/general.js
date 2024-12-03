663cdb8b2faf0520d1f1d5c97bb636d42e8ea651
Hector Valenzuela <hypermente@hotmail.com>
2024-12-03 05:25:13 -0500

    Update with promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,23 +25,48 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //get all books
-  res.send(books);
+    // Promise to get all books
+    let myPromise = new Promise ((resolve, reject) => {
+        console.log("resolving myPromise listing all books")
+        resolve(books);
+    })
+
+    //get all books
+    myPromise.then((bookList) => {
+        res.send(bookList); 
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //get specific book
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+    // Promise to get specific book
+    let myPromise = new Promise ((resolve, reject) => {
+        console.log("resolving myPromise listing specific book")
+        const isbn = req.params.isbn;
+        console.log(isbn, books[isbn]);
+        resolve(books[isbn]);
+    })
+
+    //get specific book
+    myPromise.then((bookList) => {
+        res.send(bookList);
+    })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //get books of author
-  const arrayBooks = Object.values(books);
-  const selectedBooks = arrayBooks.filter((book) => {return book.author === req.params.author})
-  res.send(selectedBooks);
+    // Promise to get books by author
+    let myPromise = new Promise ((resolve, reject) => {
+        console.log("resolving myPromise listing books by author")
+        const arrayBooks = Object.values(books);
+        const selectedBooks = arrayBooks.filter((book) => {return book.author === req.params.author})
+        resolve(selectedBooks);
+    })
+
+    //get specific book
+    myPromise.then((bookList) => {
+        res.send(bookList);
+    })
 });
 
 // Get all books based on title
